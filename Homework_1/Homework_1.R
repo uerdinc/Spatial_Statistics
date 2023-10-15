5aacb0005d89ad57615a66f33a9bccc121c77557
uerdinc <utkuerdinc1998@gmail.com>
2023-10-15 15:22:15 +0200

    Homework1

--- Homework_1/Homework_1.R
+++ Homework_1/Homework_1.R
@@ -1,59 +1,94 @@
 library(gstat)
 library(sp)
 library(geoR)
+
 data("wolfcamp")
 
+wolfcamp_df<-data.frame(wolfcamp)
+class(wolfcamp_df)
 
-class(wolfcamp)
-methods(class="geodata")
-plot(wolfcamp)
-points(wolfcamp)
-summary(wolfcamp)
-plot(variog(wolfcamp,option="cloud"),type="p")
-plot(variog(wolfcamp,option="bin", trend= "1st", max.dist = 200),type="l")
-vv<-variog(wolfcamp,option="bin", trend= "1st", max.dist = 200)
-class(vv)
-ww<-as.data.frame(wolfcamp)
-require(tidyverse)
-ggplot(ww)+
-  geom_point(aes(x=X1,y=X2,size=data))+
-  theme_light()
-ggplot(data.frame(u=vv$u,v=vv$v))+ 
-  geom_line(aes(x=u,y=v))+
-  theme_light()
+coordinates(wolfcamp_df)= ~X1+X2
 
-require(gstat)
-require(sp)
-?idw
-data("meuse.all")
-meuse<-meuse.all #save the original dataset for future  elaborations
+aq.grid
+gridded(aq.grid) = ~x+y
 
-class(meuse.all)
+#gridded(wolfcamp.grid)= ~x+y
+spplot(wolfcamp_df["data"])
+#class(wolfcamp_df$data)
 
-coordinates(meuse) = ~x+y
-#we need a grid where we'll interpolate observations
-data("meuse.grid")
-gridded(meuse.grid) = ~x+y
-meuse$zinc
-## rough exploration
-spplot(meuse["zinc"])
+class(aq.grid)
+aq.grid_df<-data.frame(aq.grid)
+class(aq.grid_df)
+#spplot(aq.grid_df)
 
-class(meuse.grid)
-class(meuse$zinc)
-meuse$lzinc<-log(meuse$zinc)
-spplot(meuse["lzinc"])
-spplot(meuse.grid)
-#simple interpolation
-x<-idw(lzinc~1,meuse, meuse.grid) #default idp=2
-x1<-idw(lzinc~1,meuse, meuse.grid,idp=1.5)
-x2<-idw(lzinc~1,meuse, meuse.grid,idp=3)
-#View(x)
+wolfcamp_df$ldata<-log(wolfcamp_df$data)
+spplot(wolfcamp_df["ldata"])
 
-spplot(x["var1.pred"])
-spplot(x1["var1.pred"])
-spplot(x2["var1.pred"])
 
-#surface plot can be made using library MBA and mba.surf interpolation
-#mean squared error is a proper score 
+
+y<-idw(ldata~1,wolfcamp_df,aq.grid)
+y1<-idw(ldata~1,wolfcamp_df,aq.grid,idp=1.5)
+y2<-idw(ldata~1,wolfcamp_df,aq.grid,idp=3)
+
+
+spplot(y["var1.pred"])
+spplot(y1["var1.pred"])
+spplot(y2["var1.pred"])
+
+
+
+y3<-idw(ldata~1,wolfcamp_df,aq.grid,idp=50)
+y4<-idw(ldata~1,wolfcamp_df,aq.grid,idp=5)
+y5<-idw(ldata~1,wolfcamp_df,aq.grid,idp=10)
+View(y3)
+
+spplot(y3["var1.pred"])
+spplot(y4["var1.pred"])
+spplot(y5["var1.pred"])
+
+
+
+library(MBA)
+?mba.surf
+
+?mba.points
+
+data(LIDAR)
+
+########################
+
+# Divide data into train and test
+
+wolfcamp_df
+
+split1<- sample(c(rep(0, 0.7 * nrow(wolfcamp_df)), rep(1, 0.3 * nrow(wolfcamp_df))))
+train <- wolfcamp_df[split1 == 0, ]    
+test <- wolfcamp_df[split1== 1, ]  
+
+table(split1)/length(wolfcamp_df)
+
+
+
+
+
+
+
+
+
+
+
+#multilevel bi-splines 
+#library(gstat)
+#library(automap)
+#wolfcamp_gstat <- gstat(id = "data", formula = data ~ 1, data = wolfcamp_df)
+#wolfcamp_kriged <- autoKrige(wolfcamp_gstat, aq.grid)
+#spplot(wolfcamp_kriged, "var1.pred")
+
+
+
+
+
+
+
 
 
