fea4aeb81ce13170da7dcb46d31cee11893e7f24
uerdinc <utkuerdinc1998@gmail.com>
2023-11-06 17:21:15 +0100

    Homework1

--- Homework_1/Homework_1.R
+++ Homework_1/Homework_1.R
@@ -1,89 +1,97 @@
-#library(gstat)
-#library(sp)
+library(sp)
+library(gstat)
 library(geoR)
+data("wolfcamp")                  # Calling libraries and wolfcamp dataset
 
-data("wolfcamp")
-class(wolfcamp)
-summary(wolfcamp)
-plot(wolfcamp)
-points(wolfcamp)
+#class(wolfcamp)                  # Checking some information about the dataset 
+#summary(wolfcamp)                #
+#plot(wolfcamp)                   #
+#points(wolfcamp)                 
+#plot(variog(wolfcamp,option="bin"),type="l")
+#plot(variog(wolfcamp,option="bin", trend = "1st"),type="l")
 
+##### Function for best P -----
 
-plot(variog(wolfcamp,option="bin"),type="l")
-plot(variog(wolfcamp,option="bin", trend = "1st"),type="l")
+wolfcamp_df<-data.frame(wolfcamp) # Converting wolfcamp data to dataframe
+coordinates(wolfcamp_df)= ~X1+X2  # Creating coordinates column 
 
+wolfcamp_df$ldata<-log(wolfcamp_df$data) # Calculating log dataset
 
+wolfcam_grid <- expand.grid(x = seq(min(wolfcamp$coords[,1]), max(wolfcamp$coords[,1])), 
+                    y = seq(min(wolfcamp$coords[,2]), max(wolfcamp$coords[,2])))          
+gridded(wolfcam_grid) = ~x+y      # Creating grid data set for IDW estimation
 
+best_p<- function(k){             # creating function for best p 
+  p = 1                           # starting p = 1 
+  mse_idw_wolfcamp<-numeric()     # creating an empty numeric data set for MSE scores
+  for (i in 1:k) {
+    idw_wolfcamp<- idw(ldata~1, wolfcamp_df, wolfcam_grid, idp = p[i]) # creating IDW model for each p value
+    mse_idw_wolfcamp[i]<-mean(idw_wolfcamp$var1.pred-wolfcamp_df$ldata)^2 # calculating MSE scores for each p value
+    p[i+1] <- p[i]+0.5          # increasing p value for upcoming IDW model
+  }
+  
+  best_p<-p[which.min(mse_idw_wolfcamp)] # selecting best p value with minimum MSE 
+  print(paste("Best p value:", best_p))  # printing best p value
 
-wolfcamp_df<-data.frame(wolfcamp)
-class(wolfcamp_df)
+} # This function can be only use for wolfcamp dataset. 
+  # Because it is creating IDW model with using wolfcamp dataset inside the function.
+  # The function does create IDW models with using different p values. 
+  # The p values starts from 1 and continues k times increasing 0.5 each time. 
+  # The k value is the function parameter that user input the function 
 
-coordinates(wolfcamp_df)= ~X1+X2
+best_p(5)  # Trial for function with k = 5 --> This mean function check for p values that p = (1, 1.5, 2, 2.5, 3)
+best_p(10) # Trial for function with k = 10 --> This mean function check for p values that p = (1, ...., 5.5)
+# best_p(c) # Trial for function with k = c --> This mean function check for p values that p = (1, ...., 0.5+(c/2))
 
-aq.grid
-gridded(aq.grid) = ~x+y
+##### IDW with 20x20 grid -----
 
-#gridded(wolfcamp.grid)= ~x+y
-spplot(wolfcamp_df["data"])
-#class(wolfcamp_df$data)
+wolfcam_grid_2 <- expand.grid(x = seq(min(wolfcamp$coords[,1]), max(wolfcamp$coords[,1]), length = 20),
+                              y = seq(min(wolfcamp$coords[,2]), max(wolfcamp$coords[,2]), length = 20))
+gridded(wolfcam_grid_2) = ~x+y            # Creating 20x20 grid dataset for IDW estimation
 
-class(aq.grid)
-aq.grid_df<-data.frame(aq.grid)
-class(aq.grid_df)
-#spplot(aq.grid_df)
+idw_wolfcamp<- idw(ldata~1, wolfcamp_df, wolfcam_grid_2, idp = 2)   # Implementing IDW model for 20x20 gridded wolfcamp data
+mse_idw_wolfcamp<-mean(idw_wolfcamp$var1.pred-wolfcamp_df$ldata)^2  # Calculating MSE for IDW model
 
-wolfcamp_df$ldata<-log(wolfcamp_df$data)
-spplot(wolfcamp_df["ldata"])
+##### Divide data into train and test -----
 
+wolfcamp_df_2<-data.frame(wolfcamp)       # Creating new wolfcamp dataframe dataset 
 
+split1<- sample(c(rep(0, 0.7 * nrow(wolfcamp_df)), rep(1, 0.3 * nrow(wolfcamp_df)))) 
+train_wolfcamp<- wolfcamp_df_2[split1 == 0, ]               # 
+test_wolfcamp <- wolfcamp_df_2[split1== 1, ]                #  Splitting wolfcamp dataset as train and test datasets
 
-y<-idw(ldata~1,wolfcamp_df,aq.grid)
-y1<-idw(ldata~1,wolfcamp_df,aq.grid,idp=1.5)
-y2<-idw(ldata~1,wolfcamp_df,aq.grid,idp=3)
+round(table(split1)/length(wolfcamp_df)*100) # Checking the percentage of train and test dataset sizes divided by length of all data
 
+##### MBA -----
 
-spplot(y["var1.pred"])
-spplot(y1["var1.pred"])
-spplot(y2["var1.pred"])
+library(MBA)                      # Calling MBA library
 
+mba_wolfcamp<-mba.surf(train_wolfcamp, no.X = 500, no.Y = 500,extend = TRUE) # Implementing mba.surf function to our train dataset
+image(mba_wolfcamp$xyz.est)
 
+persp(mba_wolfcamp$xyz.est, box = FALSE,phi = -5, theta = 125, ltheta = -125 ,
+      expand = 1 ,col = "red", shade = 1.5, border = NA)
 
-y3<-idw(ldata~1,wolfcamp_df,aq.grid,idp=50)
-y4<-idw(ldata~1,wolfcamp_df,aq.grid,idp=5)
-y5<-idw(ldata~1,wolfcamp_df,aq.grid,idp=10)
-View(y3)
+mse_mba_wolfcamp<-mean(((log(mba_wolfcamp$xyz.est$z))-(log(test_wolfcamp$data)))^2) # Calculating MSE for MBA model
 
-spplot(y3["var1.pred"])
-spplot(y4["var1.pred"])
-spplot(y5["var1.pred"])
+##### Comparing MSE values -----
 
-
-########################
-
-# Divide data into train and test
-
-wolfcamp_df
-
-split1<- sample(c(rep(0, 0.7 * nrow(wolfcamp_df)), rep(1, 0.3 * nrow(wolfcamp_df))))
-train <- wolfcamp_df[split1 == 0, ]    
-test <- wolfcamp_df[split1== 1, ]  
-
-table(split1)/length(wolfcamp_df)
-
-
-
-
-
-#multilevel bi-splines
-library(MBA)
-wolf<-wolfcamp_df
-mba.surface <- mba.surf(wolf, 300, 300, extend = TRUE)
-mba.est <- mba.surface$xyz.est
-image(mba.est, xaxs = "r", yaxs = "r")
-
+mse_table<-matrix(c(mse_idw_wolfcamp,mse_mba_wolfcamp),ncol=2)  # Creating a table for comparing MSE values for each model
+colnames(mse_table)<-c("MSE for IDW","MSE for MBA")             # Giving column names to MSE table
+mse_table
 
 
-#asdasd
+
+
+
+
+
+
+
+
+
+
+
 
 
 
